4557d545e53502b6f4d66787b015884aaddde8ae
Aye Bruh <70176152+ayebrian@users.noreply.github.com>
2022-08-25 22:12:41 +0300

    imad

--- auto.py
+++ auto.py
@@ -5,10 +5,13 @@ replacement = ""
 for line in file:
     line = line.strip()
     changes = line.replace('Alpha(DEV BRANCH)', '')
+    changes2 = line.replace('/dev/', '/main/')
     replacement = replacement + changes + '\n'
+    replacement2 = replacement + changes2 + '\n'
 
 file.close()
 # opening the file in write mode
 fout = open("modern-dark.user.css", "w")
 fout.write(replacement)
+fout.write(replacement2)
 fout.close()
